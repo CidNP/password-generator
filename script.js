6ec029978a448ee8b031652097e5ac156ab9b184
Abhishek Byanjankar <2011009_abhishek@kusom.edu.np>
2023-03-04 03:34:45 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const PwEl = document.getElementById("pw");
+const copyEl =document.getElementById("copy");
+const lenEl =document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+const numberEl = document.getElementById("number");
